4c56e42776942cabc5187fe51c3edfd10b703c0d
Sudhakar Kumar <sukumar@huronconsultinggroup.com>
2022-09-18 14:26:46 +0530

    test api project

---
+++ TestAPI/TestAPI/Controllers/Hello1Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hello1Controller : Controller
+    {
+        [HttpGet]
+       public IActionResult Get()
+        {
+            return Ok("hello world");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
